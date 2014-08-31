f82f975be277480466a7e897d2fc28b61fdca974
Jay Wick <developer@jay-wick.com>
2014-08-31 23:57:28 +1000

    Settings class

---
+++ FileSystemHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkdownLibrary
+{
+    public class FileSystemHelper
+    {
+        // return file with any parent folders created if missing
+        public static FileInfo BuildPath(string basePath, params string[] parts)
+        {
+            var nextFolder = new DirectoryInfo(basePath);
+            var folders = parts.Take(parts.Length - 1);
+
+            foreach (var item in folders)
+            {
+                nextFolder = new DirectoryInfo(Path.Combine(nextFolder.FullName, item));
+
+                if (!nextFolder.Exists)
+                    nextFolder.Create();
+            }
+
+            return new FileInfo(Path.Combine(nextFolder.FullName, parts.Last()));
+        }
+
+        public static IEnumerable<FileInfo> FindFiles(string directory, string pattern = "*.*")
+        {
+            var allFiles = Enumerable.Empty<FileInfo>();
+
+            var stack = new Stack<DirectoryInfo>();
+            stack.Push(new DirectoryInfo(directory));
+
+            while (stack.Any())
+            {
+                var current = stack.Pop();
+                var files = getFiles(current, pattern);
+                allFiles = allFiles.Concat(files);
+
+                foreach (var subdirectory in getSubdirectories(current))
+                {
+                    stack.Push(subdirectory);
+                }
+            }
+
+            return allFiles;
+        }
+
+        private static IEnumerable<FileInfo> getFiles(DirectoryInfo directory, string pattern)
+        {
+            try
+            {
+                return directory.EnumerateFiles(pattern, SearchOption.TopDirectoryOnly);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return Enumerable.Empty<FileInfo>();
+            }
+        }
+
+        private static IEnumerable<DirectoryInfo> getSubdirectories(DirectoryInfo directory)
+        {
+            try
+            {
+                return directory.EnumerateDirectories("*", SearchOption.TopDirectoryOnly);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return Enumerable.Empty<DirectoryInfo>();
+            }
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MarkdownLibrary
+{
+    [XmlType("settings")]
+    public class Settings
+    {
+        #region Singleton Access
+        private static readonly Lazy<Settings> instance = new Lazy<Settings>(() => Load());
+        public static Settings Instance { get { return instance.Value; } }
+
+        private static Settings Load()
+        {
+            var appdataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var settingsFile = FileSystemHelper.BuildPath(appdataPath, "Jay Wick Labs", "MarkdownLibrary");
+            return XmlRealizer.Realize<Settings>(settingsFile);
+        }
+        #endregion
+
+        public Settings()
+        {
+            Directory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        }
+
+        [XmlAttribute("directory")]
+        public string Directory { get; set; }
+    }
+}
