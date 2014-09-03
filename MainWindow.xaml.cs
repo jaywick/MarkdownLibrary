4c1a551cb1b45b4f5ccac3234aa6616f8ada231e
Jay Wick <developer@jay-wick.com>
2014-09-03 13:26:23 +1000

    Switched to binding to create tree
    Note: currently only shows top folders

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,105 +12,29 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.Collections.ObjectModel;
 
 namespace MarkdownLibrary
 {
     public partial class MainWindow : Window
     {
+        public Node Tree { get; set; }
+
         public MainWindow()
         {
+            createTree();
+            this.DataContext = this;
+
             InitializeComponent();
-            populateTree();
         }
 
-        private void populateTree()
+        private void createTree()
         {
-            TreeMap = new Dictionary<string, Node>();
-            foreach (var file in FileSystemHelper.FindFiles(Settings.Instance.Directory, "*.md"))
-	        {
-                addToTree(file);
-	        }
-        }
+            Tree = new Node("Root");
 
-        private void addToTree(FileInfo file)
-        {
-            var baseParts = Settings.Instance.Directory.Split('\\');
-            var currentPaths = file.FullName.Split('\\');
-
-            var lineage = currentPaths.Skip(baseParts.Length);
-
-            int i = 0;
-            dynamic next = treeFolders;
-            foreach (var part in lineage)
+            foreach (var file in new DirectoryInfo(Settings.Instance.Directory).GetDirectories())
             {
-                var currentLineage = lineage.Take(i + 1).ToArray();
-                var path = string.Join("\\", currentLineage);
-                var isLeaf = part == lineage.Last();
-
-                next = add(part, next, path, isLeaf);
-                ++i;
-            }
-        }
-
-        private TreeViewItem add(string item, dynamic parent, string path, bool isLeaf)
-        {
-            if (TreeMap.ContainsKey(path))
-                return TreeMap[path].Item;
-
-            var treeItem = new TreeViewItem
-            {
-                Header = item,
-            };
-            
-            parent.Items.Add(treeItem);
-
-            var node = new Node
-            {
-                IsLeaf = isLeaf,
-                Path = path,
-                Item = treeItem,
-            };
-
-            TreeMap.Add(path, node);
-            
-            return treeItem;
-        }
-
-        private Dictionary<string, Node> TreeMap;
-
-        private class Node
-        {
-            public bool IsLeaf { get; set; }
-            
-            public string Path { get; set; }
-
-            public TreeViewItem Item { get; set; }
-
-            public FileSystemInfo FileSystemItem
-            {
-                get
-                {
-                    var path = System.IO.Path.Combine(Settings.Instance.Directory, Path);
-
-                    if (IsLeaf)
-                    {
-                        return new FileInfo(path);
-                    }
-                    else
-                    {
-                        return new DirectoryInfo(path);
-                    }
-                }
-            }
-
-            public bool IsTopLevel
-            {
-                get { return Level == 0; }
-            }
-
-            public int Level
-            {
-                get { return Path.Split('\\').Length; }
+                Tree.Items.Add(new Node(file.Name));
             }
         }
     }
---
+++ Node.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MarkdownLibrary
+{
+    public class Node
+    {
+        public bool IsLeaf { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+
+        public Node()
+        {
+            Items = new ObservableCollection<Node>();
+        }
+
+        public Node(string name)
+            : this()
+        {
+            Name = name;
+        }
+
+        public ObservableCollection<Node> Items { get; set; }
+
+        public FileSystemInfo FileSystemItem
+        {
+            get
+            {
+                var path = System.IO.Path.Combine(Settings.Instance.Directory, Path);
+
+                if (IsLeaf)
+                {
+                    return new FileInfo(path);
+                }
+                else
+                {
+                    return new DirectoryInfo(path);
+                }
+            }
+        }
+
+        public bool IsTopLevel
+        {
+            get { return Level == 0; }
+        }
+
+        public int Level
+        {
+            get { return Path.Split('\\').Length; }
+        }
+    }
+}
